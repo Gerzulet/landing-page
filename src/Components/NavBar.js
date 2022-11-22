8d36f624705b088fbf1e48c2dec571bb7ef1c665
Gerzulet <german.alejandrozulet@gmail.com>
2022-11-22 14:57:06 -0300

    tailwind css & navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,42 @@
+import './NavBar.css'
+
+function NavBar() {
+
+
+
+
+
+  
+return(
+
+ <nav className="navbar text-white flex justify-between hover:border-solid hover:border-slate-50 border-1 w-full p-8">
+
+
+    <h1 id="logo" className="text-2xl border-solid border-3 border-white">LOGO</h1>
+    <ul className="flex text-xl justify-around mx-4">
+        <li className="mx-14" >Categorias</li>
+        <li  className="mx-5" >Contactanos</li>
+        <li  className="mx-5" >Iniciar Sesion</li>
+
+
+
+  </ul>
+
+
+
+
+
+
+
+
+
+
+  </nav>  
+
+
+
+   )
+
+  }
+
+export default NavBar;
